9efd6fc523630f087db6b63cf4cde7457f9911f6
jinggatra <wmkbisa@gmail.com>
2020-04-09 18:58:11 +0700

    blogPost

--- src/container/BlogPost/BlogPost.jsx
+++ src/container/BlogPost/BlogPost.jsx
@@ -4,7 +4,18 @@ import './BlogPost.css';
 class BlogPost extends Component{
     render(){
         return(
-            <p>Blog Artikel</p>
+            <div class="post-artikel">
+                <h2>Daftar Artikel</h2>
+                <div class="artikel">
+                    <div class="gambar-artikel">
+                        <img src="http://placeimg.com " alt="Gambar Thumbnail Artikel"/>
+                    </div>
+                    <div class="konten-artikel">
+                        <div class="judul-artikel">Judul Artikel</div>
+                        <p class="list-artikel">Isi Artikel</p>
+                    </div>
+                </div>
+            </div>
         )
     }
 }
