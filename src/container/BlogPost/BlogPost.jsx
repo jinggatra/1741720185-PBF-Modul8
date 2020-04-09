890ec0675fe6f3957af1fe0ab0ee9486d5aeba1d
jinggatra <wmkbisa@gmail.com>
2020-04-09 19:06:38 +0700

    post

--- src/container/BlogPost/BlogPost.jsx
+++ src/container/BlogPost/BlogPost.jsx
@@ -1,20 +1,13 @@
 import React, {Component} from 'react';
 import './BlogPost.css';
+import Post from '../BlogPost/Post';
 
 class BlogPost extends Component{
     render(){
         return(
             <div class="post-artikel">
                 <h2>Daftar Artikel</h2>
-                <div class="artikel">
-                    <div class="gambar-artikel">
-                        <img src="http://placeimg.com " alt="Gambar Thumbnail Artikel"/>
-                    </div>
-                    <div class="konten-artikel">
-                        <div class="judul-artikel">Judul Artikel</div>
-                        <p class="list-artikel">Isi Artikel</p>
-                    </div>
-                </div>
+                <Post/>
             </div>
         )
     }
