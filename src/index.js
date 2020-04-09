74d8736fb52147675b090309ea26ae1f1f4ed0d4
jinggatra <wmkbisa@gmail.com>
2020-04-09 19:11:27 +0700

    coba push

--- src/index.js
+++ src/index.js
@@ -1,20 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
-// import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import * as serviceWorker from './serviceWorker';
-import BlogPost from './container/BlogPost/BlogPost';
 
+import BlogPost from "./container/BlogPost/BlogPost";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <BlogPost />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<BlogPost />, document.getElementById('content'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
